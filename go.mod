2da4dc7fc5fd5a4885941e67e88e5194667cca92
Sophearak Tha <t.sophearak@gmail.com>
2019-03-23 13:19:13 +0700

    should work

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sophearak/angkor.dev
+
+require github.com/mattes/go-asciibot v0.0.0-20190314200458-26c3a522dfb9
---
+++ index.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/mattes/go-asciibot"
+)
+
+// H handler function
+func H(w http.ResponseWriter, r *http.Request) {
+	robot(w, r)
+
+	rf := r.Header.Get("Referer")
+
+	if strings.Contains(rf, "khmer.dev") {
+		template(w)
+		return
+	}
+
+	http.Redirect(w, r, "https://khmer.dev", 307)
+}
+
+func robot(w http.ResponseWriter, r *http.Request) {
+	ua := r.Header.Get("User-Agent")
+
+	if strings.Contains(ua, "curl") || strings.Contains(ua, "HTTPie") {
+		bot := asciibot.Random()
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Length", strconv.Itoa(len(bot)))
+
+		w.Write([]byte(bot))
+	}
+}
+
+func template(w http.ResponseWriter) {
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>{ angkor.dev }</title>
+		<style>
+			body {
+				background: #111;
+				margin: 0;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				height: 100vh;
+			}
+			img {
+				width: 200px;
+				height: auto;
+			}
+		</style>
+	</head>
+	<body>
+		<img src="/angkor.dev.png" alt="angkor.dev">
+	</body>
+	</html>
+	`
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", strconv.Itoa(len(html)))
+
+	w.Write([]byte(html))
+}
